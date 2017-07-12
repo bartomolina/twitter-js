2e287c39e718c93356c8273de991bbd6d4b33c43
Barto Molina <bartomolina@gmail.com>
2017-07-11 21:55:40 -0400

    final commit - tuesday

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require( 'express' );
 const volleyball = require( 'volleyball' );
+const nunjucks = require( 'nunjucks' );
 const app = express();
 
 var port = process.env.PORT || 3000;
@@ -7,6 +8,10 @@ var port = process.env.PORT || 3000;
 console.log("start");
 
 app.use(volleyball);
+nunjucks.configure('views', {noCache: true});
+
+
+nunjucks.render('index.html', { title: "Outside request"}, function(err, output){console.log(output)});
 
 // app.use(function(req, res, next) {
 //     console.log(req.method, req.url);
@@ -23,7 +28,8 @@ app.listen (port, function() {
 });
 
 app.get("/", function(request, response, next){
-    response.send("hello");
+    var res = nunjucks.render('index.html', { title: "An Example", people: [{name: "Gandal"}, {name: "Frodo"}, {name: "Hermione"}] });
+    response.send(res);
 });
 
 app.get("/news", function(request, response, next){
