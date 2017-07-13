30a0b1b00047d3459d573f2a8f9f12b3b5c26ed2
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-07-12 21:05:54 -0400

    up to live test

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require( 'express' );
 const volleyball = require( 'volleyball' );
 const nunjucks = require( 'nunjucks' );
+const routes = require('./routes');
 const app = express();
 
 
+app.use(express.static('public'));
 app.use(volleyball);
+app.use('/', routes);
 
 app.engine("html", nunjucks.render);
 app.set("view engine", "html");
@@ -12,26 +15,7 @@ nunjucks.configure('views', { noCache: true });
 
 var port = process.env.PORT || 3000;
 
-// app.use(function(req, res, next) {
-//     console.log(req.method, req.url);
-//     next();
-// });
-
-// app.use("/news", function(req, res, next) {
-//     console.log("you've reached news");
-//     next();
-// });
 
 app.listen (port, function() {
     console.log(`listening on port ${ port }`);
-});
-
-app.get("/", function(request, response, next){
-    response.render('index', { title: "An Example", people: [{name: "Gandal"}, {name: "Frodo"}, {name: "Hermione"}] })
-    // var res = nunjucks.render('index.html', { title: "An Example", people: [{name: "Gandal"}, {name: "Frodo"}, {name: "Hermione"}] });
-    response.send(res);
-});
-
-app.get("/news", function(request, response, next){
-    response.send("news");
 });
